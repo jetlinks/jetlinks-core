fad55412fa4037bbf455efc5b6f908250adb1ea2
zhou-hao <admin@hsweb.me>
2019-05-29 10:32:12 +0800

    优化注释

--- src/main/java/org/jetlinks/core/device/DeviceMessageSender.java
+++ src/main/java/org/jetlinks/core/device/DeviceMessageSender.java
@@ -14,6 +14,17 @@ import java.util.function.Supplier;
  */
 public interface DeviceMessageSender {
 
+    /**
+     * 尝试获取设备已经回复,但是未及时获取对设备返回对消息. 如果设备未回复或者回复的消息已经超时
+     * 将会得到结果: {"success":false,"code":"NO_REPLY"}
+     *
+     * @param deviceId 设备ID
+     * @param messageId 消息ID
+     * @param replyNewInstance 回复对象提供者
+     * @param <R> 回复类型
+     * @return 异步获取结果
+     * @see org.jetlinks.core.enums.ErrorCode#NO_REPLY
+     */
     <R extends DeviceMessageReply> CompletionStage<R> retrieveReply(String deviceId, String messageId, Supplier<R> replyNewInstance);
 
     /**
@@ -21,6 +32,7 @@ public interface DeviceMessageSender {
      * <p>
      * ⚠️: 请勿自己实现消息对象,而应该使用框架定义的3种消息.
      * ⚠️: 如果是异步消息,将直接返回<code>{"success":false,"code":"REQUEST_HANDLING"}</code>
+     *
      * @param message 具体的消息对象
      * @param <R>     返回类型
      * @return 异步发送结果
@@ -34,7 +46,18 @@ public interface DeviceMessageSender {
      * @see org.jetlinks.core.enums.ErrorCode#REQUEST_HANDLING
      */
     <R extends DeviceMessageReply> CompletionStage<R> send(RepayableDeviceMessage<R> message);
-    public <R extends DeviceMessageReply> CompletionStage<R> send(DeviceMessage message, Function<Object, R> replyMapping);
+
+    /**
+     * 发送消息并自定义返回结果转换器
+     *
+     * @param message      消息
+     * @param replyMapping 消息回复转换器
+     * @param <R>          回复类型
+     * @return 异步发送结果
+     * @see this#send(RepayableDeviceMessage)
+     */
+    <R extends DeviceMessageReply> CompletionStage<R> send(DeviceMessage message, Function<Object, R> replyMapping);
+
     /**
      * 发送{@link org.jetlinks.core.message.function.FunctionInvokeMessage}消息更便捷的API
      *
@@ -52,7 +75,7 @@ public interface DeviceMessageSender {
      * @param property 要获取的属性列表
      * @return ReadPropertyMessageSender
      * @see this#send(RepayableDeviceMessage)
-     * @see  org.jetlinks.core.message.property.ReadPropertyMessage
+     * @see org.jetlinks.core.message.property.ReadPropertyMessage
      * @see ReadPropertyMessageSender
      */
     ReadPropertyMessageSender readProperty(String... property);
@@ -62,7 +85,7 @@ public interface DeviceMessageSender {
      *
      * @return WritePropertyMessageSender
      * @see this#send(RepayableDeviceMessage)
-     * @see  org.jetlinks.core.message.property.WritePropertyMessage
+     * @see org.jetlinks.core.message.property.WritePropertyMessage
      * @see WritePropertyMessageSender
      */
     WritePropertyMessageSender writeProperty();
--- src/main/java/org/jetlinks/core/message/function/FunctionInvokeMessageReply.java
+++ src/main/java/org/jetlinks/core/message/function/FunctionInvokeMessageReply.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.jetlinks.core.message.CommonDeviceMessageReply;
 
+
 /**
  * @author zhouhao
  * @since 1.0.0
