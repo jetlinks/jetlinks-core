c86a59a2e49e0abaf33c6bdb015fa1b4cd096843
zhou-hao <admin@hsweb.me>
2019-06-25 16:55:59 +0800

    优化测试

--- src/test/java/org/jetlinks/core/metadata/DefaultValueWrapperTest.java
+++ src/test/java/org/jetlinks/core/metadata/DefaultValueWrapperTest.java
@@ -10,9 +10,22 @@ public class DefaultValueWrapperTest {
     @Test
     public void testInt() {
 
-        ValueWrapper valueWrapper=new DefaultValueWrapper(Long.MAX_VALUE);
+        ValueWrapper valueWrapper = new DefaultValueWrapper(Long.MAX_VALUE);
         Assert.assertTrue(valueWrapper.asInteger().isPresent());
 
+        Assert.assertTrue(valueWrapper.as(Long.class).isPresent());
+
+    }
+
+    @Test
+    public void testList() {
+
+        ValueWrapper valueWrapper = new DefaultValueWrapper("[1,2,3]");
+        int val = valueWrapper.asList(Integer.class)
+                .map(list -> list.stream().mapToInt(Integer::intValue).sum())
+                .orElse(0);
+
+        Assert.assertEquals(val, 6);
 
     }
 
