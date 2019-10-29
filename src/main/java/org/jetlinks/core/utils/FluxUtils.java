e209e13d40d9a489cd809e013076960490caa5cf
zhou-hao <admin@hsweb.me>
2019-10-29 19:08:56 +0800

    优化

--- src/main/java/org/jetlinks/core/utils/FluxUtils.java
+++ src/main/java/org/jetlinks/core/utils/FluxUtils.java
@@ -1,31 +1,34 @@
 package org.jetlinks.core.utils;
 
 import reactor.core.publisher.Flux;
-import reactor.util.function.Tuple2;
 
 import java.time.Duration;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class FluxUtils {
 
     public static <T> Flux<List<T>> bufferRate(Flux<T> flux, int rate, Duration maxTimeout) {
-        return Flux.create(sink -> sink.onDispose(flux.elapsed()
-                .bufferWhile(r -> {
-                    if (r.getT1() < rate) {
-                        return true;
-                    }
-                    sink.next(Collections.singletonList(r.getT2()));
+        return bufferRate(flux, rate, 100, maxTimeout);
+    }
 
-                    return false;
-                })
-                .map(list -> list.stream().map(Tuple2::getT2).collect(Collectors.toList()))
-                .bufferTimeout(10, maxTimeout)
-                .doOnError(sink::error)
-                .doOnComplete(sink::complete)
-                .flatMap(Flux::fromIterable)
-                .subscribe(sink::next)));
+    public static <T> Flux<List<T>> bufferRate(Flux<T> flux, int rate, int maxSize, Duration maxTimeout) {
+
+        return Flux.create(sink -> sink.onDispose(Flux.<T>create(
+                buffer ->
+                        buffer.onDispose(
+                                flux.elapsed()
+                                        .subscribe(e -> {
+                                            if (e.getT1() > rate) {
+                                                sink.next(Collections.singletonList(e.getT2()));
+                                            } else {
+                                                buffer.next(e.getT2());
+                                            }
+                                        })))
+                .bufferTimeout(maxSize, maxTimeout)
+                .subscribe(sink::next)
+        )
+        );
     }
 
 }
