73897bb0ad19961dd96b3c0af8200e8b1b73f8cc
zhou-hao <admin@hsweb.me>
2020-03-11 13:33:15 +0800

    优化bytes util

--- src/main/java/org/jetlinks/core/utils/BytesUtils.java
+++ src/main/java/org/jetlinks/core/utils/BytesUtils.java
@@ -8,7 +8,7 @@ package org.jetlinks.core.utils;
 public class BytesUtils {
 
     /**
-     * 高位字节数组转int,低字节在前.高位在后.
+     * 小端模式转int,低字节在前.高在后.
      * -------------------------------------
      * | 0-7位 | 8-15位 | 16-23位 | 24-31位 |
      * -------------------------------------
@@ -18,7 +18,7 @@ public class BytesUtils {
      * @param len    长度
      * @return 转换结果
      */
-    public static int highBytesToInt(byte[] src, int offset, int len) {
+    public static int leToInt(byte[] src, int offset, int len) {
         int n = 0;
         len = Math.min(len, 4);
         for (int i = 0; i < len; i++) {
@@ -28,8 +28,12 @@ public class BytesUtils {
         return n;
     }
 
+    public static int leToInt(byte[] src) {
+        return leToInt(src, 0, src.length);
+    }
+
     /**
-     * 高位字节数组转long,低字节在前.高位在后.
+     * 小端模式转long,低字节在前.高在后.
      * ---------------------------------------------------------------------------
      * | 0-7位 | 8-15位 | 16-23位 | 24-31位 | 32-39位 | 40-47位 | 48-55位 | 56-63位 |
      * ---------------------------------------------------------------------------
@@ -39,7 +43,7 @@ public class BytesUtils {
      * @param len    长度
      * @return 转换结果
      */
-    public static long highBytesToLong(byte[] src, int offset, int len) {
+    public static long leToLong(byte[] src, int offset, int len) {
         long n = 0;
         len = Math.min(Math.min(len, src.length), 8);
         for (int i = 0; i < len; i++) {
@@ -49,17 +53,32 @@ public class BytesUtils {
         return n;
     }
 
+    public static long leToLong(byte[] src) {
+        return highBytesToLong(src, 0, src.length);
+    }
+
+    @Deprecated
+    public static int highBytesToInt(byte[] src, int offset, int len) {
+        return leToInt(src, offset, len);
+    }
+
+    @Deprecated
+    public static long highBytesToLong(byte[] src, int offset, int len) {
+        return leToLong(src, offset, len);
+    }
+    @Deprecated
     public static int highBytesToInt(byte[] src) {
         return highBytesToInt(src, 0, src.length);
     }
 
+    @Deprecated
     public static long highBytesToLong(byte[] src) {
         return highBytesToLong(src, 0, src.length);
     }
 
 
     /**
-     * IEEE 754 字节数组转float, 低位字节在前,高位字节在后.
+     * 小端模式,IEEE 754 字节数组转float,低位字节在前,高位字节在后.
      *
      * @param src    数组
      * @param offset 偏移量
@@ -68,12 +87,16 @@ public class BytesUtils {
      * @see this#highBytesToInt(byte[], int, int)
      * @see Float#intBitsToFloat(int)
      */
-    public static float highBytesToFloat(byte[] src, int offset, int len) {
-        return Float.intBitsToFloat(highBytesToInt(src, offset, len));
+    public static float leToFloat(byte[] src, int offset, int len) {
+        return Float.intBitsToFloat(leToInt(src, offset, len));
+    }
+
+    public static float leToFloat(byte[] src) {
+        return leToFloat(src, 0, src.length);
     }
 
     /**
-     * IEEE 754 字节数组转double, 低位字节在前,高位字节在后.
+     * 小端模式,IEEE 754 字节数组转double,低位字节在前,高位字节在后.
      *
      * @param src    数组
      * @param offset 偏移量
@@ -81,20 +104,36 @@ public class BytesUtils {
      * @return 转换结果
      * @see this#highBytesToLong(byte[], int, int)
      */
+    public static double leToDouble(byte[] src, int offset, int len) {
+        return Double.longBitsToDouble(leToLong(src, offset, len));
+    }
+
+    public static double leToDouble(byte[] src) {
+        return leToDouble(src, 0, src.length);
+    }
+
+    @Deprecated
+    public static float highBytesToFloat(byte[] src, int offset, int len) {
+        return leToFloat(src, offset, len);
+    }
+
+    @Deprecated
     public static double highBytesToDouble(byte[] src, int offset, int len) {
         return Double.longBitsToDouble(highBytesToLong(src, offset, len));
     }
 
+    @Deprecated
     public static float highBytesToFloat(byte[] src) {
-        return highBytesToFloat(src, 0, src.length);
+        return leToFloat(src, 0, src.length);
     }
 
+    @Deprecated
     public static double highBytesToDouble(byte[] src) {
         return highBytesToDouble(src, 0, src.length);
     }
 
     /**
-     * 低位字节数组转int,高位字节在前,低字节在后.
+     * 大端模式,字节数组转int,高位字节在前,低字节在后.
      * ------------------------------------
      * | 31-24位 | 23-16位 | 15-8位 | 7-0位 |
      * ------------------------------------
@@ -104,7 +143,7 @@ public class BytesUtils {
      * @param len    长度
      * @return int值
      */
-    public static int lowBytesToInt(byte[] src, int offset, int len) {
+    public static int beToInt(byte[] src, int offset, int len) {
         int n = 0;
         len = Math.min(Math.min(len, src.length), 4);
         for (int i = 0; i < len; i++) {
@@ -112,6 +151,11 @@ public class BytesUtils {
             n += ((src[offset + len - i - 1] & 0xFF) << left);
         }
         return n;
+    }
+
+    @Deprecated
+    public static int lowBytesToInt(byte[] src, int offset, int len) {
+        return beToInt(src, offset, len);
     }
 
     /**
@@ -125,7 +169,7 @@ public class BytesUtils {
      * @param len    长度
      * @return int值
      */
-    public static long lowBytesToLong(byte[] src, int offset, int len) {
+    public static long beToLong(byte[] src, int offset, int len) {
         long n = 0;
         len = Math.min(Math.min(len, src.length), 8);
         for (int i = 0; i < len; i++) {
@@ -135,12 +179,28 @@ public class BytesUtils {
         return n;
     }
 
+
+    public static float beToFloat(byte[] src, int offset, int len) {
+        return Float.intBitsToFloat(beToInt(src, offset, len));
+    }
+
+    public static double beToDouble(byte[] src, int offset, int len) {
+        return Double.longBitsToDouble(beToLong(src, offset, len));
+    }
+
+    @Deprecated
+    public static long lowBytesToLong(byte[] src, int offset, int len) {
+        return beToLong(src, offset, len);
+    }
+
+    @Deprecated
     public static float lowBytesToFloat(byte[] src, int offset, int len) {
         return Float.intBitsToFloat(lowBytesToInt(src, offset, len));
     }
 
+    @Deprecated
     public static double lowBytesToDouble(byte[] src, int offset, int len) {
-        return Double.longBitsToDouble(lowBytesToLong(src, offset, len));
+        return beToDouble(src, offset, len);
     }
 
 
@@ -153,73 +213,161 @@ public class BytesUtils {
      * @param src 字节数组
      * @return int值
      */
+    public static int beToInt(byte[] src) {
+        return beToInt(src, 0, src.length);
+    }
+
+    @Deprecated
     public static int lowBytesToInt(byte[] src) {
         return lowBytesToInt(src, 0, src.length);
     }
 
+    public static long beToLong(byte[] src) {
+        return beToLong(src, 0, src.length);
+    }
+
+    @Deprecated
     public static long lowBytesToLong(byte[] src) {
         return lowBytesToLong(src, 0, src.length);
     }
 
+    public static float beToFloat(byte[] src) {
+        return beToFloat(src, 0, src.length);
+    }
+
+    @Deprecated
     public static float lowBytesToFloat(byte[] src) {
         return lowBytesToFloat(src, 0, src.length);
     }
 
+    public static double beToDouble(byte[] src) {
+        return beToDouble(src, 0, src.length);
+    }
+
+    @Deprecated
     public static double lowBytesToDouble(byte[] src) {
         return lowBytesToDouble(src, 0, src.length);
     }
 
 
     /**
-     * int转高位字节数组,低字节在前
+     * int转小端模式字节数组,低字节在前
      * ---------------------------------------
      * | 0-7位 | 8-15位 | 16-23位  |  24-31位 |
      * ---------------------------------------
      *
-     * @param src 字节数组
-     * @return bytes 值
+     * @param number 数字值
+     * @param target 目标数组
+     * @param offset 偏移量
+     * @param len    总长度
+     * @return bytes 原始值
      */
-    public static byte[] toHighBytes(byte[] target, long src, int offset, int len) {
+    public static byte[] numberToLe(byte[] target, Number number, int offset, int len) {
+        long src = number.longValue();
         for (int i = 0; i < len; i++) {
             target[offset + i] = (byte) (src >> (i * 8) & 0xff);
         }
         return target;
     }
 
+    @Deprecated
+    public static byte[] toHighBytes(byte[] target, long src, int offset, int len) {
+        return numberToBe(target, src, offset, len);
+    }
+
     /**
-     * int转高位字节数组,低字节在前
+     * int转小端模式字节数组,低字节在前
      * -------------------------------------------
      * |  0-7位  |  8-15位  |  16-23位  |  24-31位 |
      * -------------------------------------------
      *
-     * @param src 字节数组
-     * @return bytes 值
+     * @param number 数字值
+     * @param target 目标数组
+     * @param offset 填充偏移量
+     * @return bytes 原始值
      */
+    public static byte[] intToLe(byte[] target, int number, int offset) {
+        return numberToLe(target, number, offset, 4);
+    }
+
+    public static byte[] intToLe(int src) {
+        return intToLe(new byte[4], src, 0);
+    }
+
+    public static byte[] shortToLe(byte[] target, short number, int offset) {
+        return numberToLe(target, number, offset, 2);
+    }
+
+    public static byte[] shortToLe(short src) {
+        return shortToLe(new byte[2], src, 0);
+    }
+
+    public static byte[] longToLe(byte[] target, long number, int offset) {
+        return numberToLe(target, number, offset, 8);
+    }
+
+    public static byte[] longToLe(long src) {
+        return longToLe(new byte[8], src, 0);
+    }
+
+    public static byte[] floatToLe(byte[] target, float number, int offset) {
+        return intToBe(target, Float.floatToIntBits(number), offset);
+    }
+
+    public static byte[] floatToLe(float src) {
+        return intToLe(Float.floatToIntBits(src));
+    }
+
+    public static byte[] doubleToLe(byte[] target, double number, int offset) {
+        return longToLe(target, Double.doubleToLongBits(number), offset);
+    }
+
+    public static byte[] doubleToLe(double src) {
+        return longToLe(Double.doubleToLongBits(src));
+    }
+
+    @Deprecated
     public static byte[] toHighBytes(int src) {
-        return toHighBytes(new byte[4], src, 0, 4);
+        return intToLe(src);
     }
 
+    @Deprecated
     public static byte[] toHighBytes(long src) {
-        return toHighBytes(new byte[8], src, 0, 8);
+        return longToLe(src);
     }
 
+    @Deprecated
     public static byte[] toHighBytes(double src) {
-        return toHighBytes(Double.doubleToLongBits(src));
+        return doubleToLe(src);
     }
 
+    @Deprecated
     public static byte[] toHighBytes(float src) {
-        return toHighBytes(Float.floatToIntBits(src));
+        return intToLe(Float.floatToIntBits(src));
     }
+
 
     /**
-     * 转低位字节数组, 低字节在后
+     * 数字大端模式转字节数组, 低字节在后
      * -------------------------------------
      * | 31-24位 | 23-16位 | 15-8位 | 7-0位 |
      * -------------------------------------
      *
-     * @param src 字节数组
+     * @param number 数字值
+     * @param target 目标数组
+     * @param offset 偏移量
+     * @param len    总长度
      * @return int值
      */
+    public static byte[] numberToBe(byte[] target, Number number, int offset, int len) {
+        long src = number.longValue();
+        for (int i = 0; i < len; i++) {
+            target[offset + len - i - 1] = (byte) (src >> (i * 8) & 0xff);
+        }
+        return target;
+    }
+
+    @Deprecated
     public static byte[] toLowBytes(byte[] target, long src, int offset, int len) {
         for (int i = 0; i < len; i++) {
             target[offset + len - i - 1] = (byte) (src >> (i * 8) & 0xff);
@@ -227,31 +375,104 @@ public class BytesUtils {
         return target;
     }
 
+
     /**
      * int转低位字节数组, 低字节在后
      * --------------------------------------------
      * |  31-24位 |  23-16位   |  15-8位 |   7-0位 |
      * --------------------------------------------
      *
-     * @param src 字节数组
-     * @return int值
+     * @param number 数字值
+     * @param target 目标数组
+     * @param offset 偏移量
+     * @return bytes 原始值
      */
+    public static byte[] intToBe(byte[] target, int number, int offset) {
+        return numberToBe(target, number, offset, 4);
+    }
+
+    public static byte[] intToBe(int src) {
+        return intToBe(new byte[4], src, 0);
+    }
+
+    public static byte[] shortToBe(byte[] target, short number, int offset) {
+        return numberToBe(target, number, offset, 2);
+    }
+
+    public static byte[] shortToBe(short src) {
+        return shortToBe(new byte[2], src, 0);
+    }
+
+    public static byte[] longToBe(byte[] target, long number, int offset) {
+        return numberToBe(target, number, offset, 8);
+    }
+
+    public static byte[] longToBe(long src) {
+        return longToBe(new byte[8], src, 0);
+    }
+
+    public static byte[] floatToBe(byte[] target, float number, int offset) {
+        return intToBe(target, Float.floatToIntBits(number), offset);
+    }
+
+    public static byte[] floatToBe(float src) {
+        return intToBe(Float.floatToIntBits(src));
+    }
+
+    public static byte[] doubleToBe(byte[] target, double number, int offset) {
+        return longToBe(target, Double.doubleToLongBits(number), offset);
+    }
+
+    public static byte[] doubleToBe(double src) {
+        return longToBe(Double.doubleToLongBits(src));
+    }
+
+    /**
+     * 反转数组. [4,3,2,1] => [1,2,3,4]
+     *
+     * @param data   数据
+     * @param offset 偏移量
+     * @param length 长度
+     * @return 转换后到数组
+     */
+    public static byte[] reverse(byte[] data, int offset, int length) {
+
+        int len = Math.min(data.length - 1, length);
+        for (int i = 0; i < length / 2; i++) {
+            int idx = i + offset;
+            int rev = len - i;
+            byte temp = data[idx];
+            data[idx] = data[rev];
+            data[rev] = temp;
+        }
+
+        return data;
+    }
+
+    public static byte[] reverse(byte[] data) {
+        return reverse(data, 0, data.length);
+    }
+
+
+    @Deprecated
     public static byte[] toLowBytes(int src) {
         return toLowBytes(new byte[4], src, 0, 4);
     }
 
-
+    @Deprecated
     public static byte[] toLowBytes(long src) {
         return toLowBytes(new byte[8], src, 0, 8);
     }
 
-
+    @Deprecated
     public static byte[] toLowBytes(double src) {
         return toLowBytes(Double.doubleToLongBits(src));
     }
 
+    @Deprecated
     public static byte[] toLowBytes(float src) {
         return toLowBytes(Float.floatToIntBits(src));
     }
+
 
 }
--- src/test/java/org/jetlinks/core/utils/BytesUtilsTest.java
+++ src/test/java/org/jetlinks/core/utils/BytesUtilsTest.java
@@ -5,18 +5,16 @@ import org.junit.Test;
 
 import java.util.Random;
 
-import static org.junit.Assert.*;
-
 public class BytesUtilsTest {
 
     @Test
     public void testInt() {
-        Assert.assertEquals(BytesUtils.highBytesToInt(BytesUtils.toHighBytes(32)), 32);
-        Assert.assertEquals(BytesUtils.lowBytesToInt(BytesUtils.toLowBytes(32)), 32);
+        Assert.assertEquals(BytesUtils.leToInt(BytesUtils.intToLe(32)), 32);
+        Assert.assertEquals(BytesUtils.leToInt(BytesUtils.intToLe(32)), 32);
 
-        Assert.assertEquals(BytesUtils.lowBytesToInt(BytesUtils.toLowBytes(Short.MIN_VALUE)), Short.MIN_VALUE);
-        Assert.assertEquals(BytesUtils.lowBytesToInt(BytesUtils.toLowBytes(Integer.MIN_VALUE)), Integer.MIN_VALUE);
-        Assert.assertEquals(BytesUtils.lowBytesToInt(BytesUtils.toLowBytes(Integer.MAX_VALUE)), Integer.MAX_VALUE);
+        Assert.assertEquals(BytesUtils.leToInt(BytesUtils.intToLe(Short.MIN_VALUE)), Short.MIN_VALUE);
+        Assert.assertEquals(BytesUtils.leToInt(BytesUtils.intToLe(Integer.MIN_VALUE)), Integer.MIN_VALUE);
+        Assert.assertEquals(BytesUtils.leToInt(BytesUtils.intToLe(Integer.MAX_VALUE)), Integer.MAX_VALUE);
 
     }
 
@@ -25,15 +23,15 @@ public class BytesUtilsTest {
         {
             long val = new Random().nextLong();
 
-            Assert.assertEquals(BytesUtils.highBytesToLong(BytesUtils.toHighBytes(val)), val);
-            Assert.assertEquals(BytesUtils.lowBytesToLong(BytesUtils.toLowBytes(val)), val);
+            Assert.assertEquals(BytesUtils.leToLong(BytesUtils.longToLe(val)), val);
+            Assert.assertEquals(BytesUtils.beToLong(BytesUtils.longToBe(val)), val);
         }
 
         {
             long val = -new Random().nextLong();
 
-            Assert.assertEquals(BytesUtils.highBytesToLong(BytesUtils.toHighBytes(val)), val);
-            Assert.assertEquals(BytesUtils.lowBytesToLong(BytesUtils.toLowBytes(val)), val);
+            Assert.assertEquals(BytesUtils.leToLong(BytesUtils.longToLe(val)), val);
+            Assert.assertEquals(BytesUtils.beToLong(BytesUtils.longToBe(val)), val);
         }
     }
 
@@ -41,13 +39,13 @@ public class BytesUtilsTest {
     public void testDouble() {
         {
             double val = new Random().nextDouble();
-            Assert.assertEquals(BytesUtils.highBytesToDouble(BytesUtils.toHighBytes(val)), val, 0);
-            Assert.assertEquals(BytesUtils.lowBytesToDouble(BytesUtils.toLowBytes(val)), val, 0);
+            Assert.assertEquals(BytesUtils.leToDouble(BytesUtils.doubleToLe(val)), val, 0);
+            Assert.assertEquals(BytesUtils.beToDouble(BytesUtils.doubleToBe(val)), val, 0);
         }
         {
             double val = -new Random().nextDouble();
-            Assert.assertEquals(BytesUtils.highBytesToDouble(BytesUtils.toHighBytes(val)), val, 0);
-            Assert.assertEquals(BytesUtils.lowBytesToDouble(BytesUtils.toLowBytes(val)), val, 0);
+            Assert.assertEquals(BytesUtils.leToDouble(BytesUtils.doubleToLe(val)), val, 0);
+            Assert.assertEquals(BytesUtils.beToDouble(BytesUtils.doubleToBe(val)), val, 0);
         }
     }
 
@@ -55,31 +53,61 @@ public class BytesUtilsTest {
     public void testFloat() {
         {
             float val = new Random().nextFloat();
-            Assert.assertEquals(BytesUtils.highBytesToFloat(BytesUtils.toHighBytes(val)), val, 0);
-            Assert.assertEquals(BytesUtils.lowBytesToFloat(BytesUtils.toLowBytes(val)), val, 0);
+            Assert.assertEquals(BytesUtils.leToFloat(BytesUtils.floatToLe(val)), val, 0);
+            Assert.assertEquals(BytesUtils.beToFloat(BytesUtils.floatToBe(val)), val, 0);
         }
         {
             float val = -new Random().nextFloat();
-            Assert.assertEquals(BytesUtils.highBytesToFloat(BytesUtils.toHighBytes(val)), val, 0);
-            Assert.assertEquals(BytesUtils.lowBytesToFloat(BytesUtils.toLowBytes(val)), val, 0);
+            Assert.assertEquals(BytesUtils.leToFloat(BytesUtils.floatToLe(val)), val, 0);
+            Assert.assertEquals(BytesUtils.beToFloat(BytesUtils.floatToBe(val)), val, 0);
         }
     }
 
     @Test
     public void testOffset() {
 
-        byte[] data = BytesUtils.toLowBytes(new byte[4], 566, 2, 2);
+        byte[] data = BytesUtils.numberToLe(new byte[4], 566, 2, 2);
 
-        Assert.assertEquals(BytesUtils.lowBytesToInt(data, 2, 2), 566);
+        Assert.assertEquals(BytesUtils.leToInt(data, 2, 2), 566);
 
-        Assert.assertEquals(BytesUtils.lowBytesToInt(new byte[]{2, 1, 0, 2}, 1, 2), 256);
+        Assert.assertEquals(BytesUtils.beToInt(new byte[]{2, 1, 0, 2}, 1, 2), 256);
 
-        Assert.assertEquals(BytesUtils.lowBytesToInt(new byte[]{0, 1, 2, 2}, 0, 2), 1);
+        Assert.assertEquals(BytesUtils.beToInt(new byte[]{0, 1, 2, 2}, 0, 2), 1);
 
-        Assert.assertEquals(BytesUtils.highBytesToLong(new byte[]{0, 1, 1, 1}, 0, 2), 256);
-        data = BytesUtils.toHighBytes(new byte[4], 1045, 1, 2);
+        Assert.assertEquals(BytesUtils.leToLong(new byte[]{0, 1, 1, 1}, 0, 2), 256);
+        data = BytesUtils.numberToLe(new byte[4], 1045, 1, 2);
 
-        Assert.assertEquals(BytesUtils.highBytesToLong(data, 1, 2), 1045);
+        Assert.assertEquals(BytesUtils.leToInt(data, 1, 2), 1045);
+
+    }
+
+    @Test
+    public void testReverse() {
+        {
+            byte[] arr = new byte[]{1, 2, 3, 4};
+            Assert.assertArrayEquals(BytesUtils.reverse(arr), new byte[]{4, 3, 2, 1});
+        }
+
+        {
+            byte[] arr = new byte[]{1, 2, 3, 4, 5};
+            Assert.assertArrayEquals(BytesUtils.reverse(arr, 1, 2), new byte[]{1, 3, 2, 4, 5});
+        }
+
+        {
+            byte[] arr = new byte[]{1, 2, 3, 4, 5};
+            Assert.assertArrayEquals(BytesUtils.reverse(arr, 1, 3), new byte[]{1, 4, 3, 2, 5});
+        }
+
+        {
+            byte[] arr2 = new byte[]{1, 2, 3, 4, 5};
+            Assert.assertArrayEquals(BytesUtils.reverse(arr2, 1, 4), new byte[]{1, 5, 4, 3, 2});
+        }
+
+        {
+            byte[] arr2 = new byte[]{1, 2, 3, 4, 5};
+            Assert.assertArrayEquals(BytesUtils.reverse(arr2, 2, 4), new byte[]{1, 2, 5, 4, 3});
+        }
+
 
     }
 }
