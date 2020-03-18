17e11f9ea7b7582679415e342b4afc8aac1b3c8d
zhou-hao <admin@hsweb.me>
2020-03-18 15:14:57 +0800

    优化类型转换

--- src/main/java/org/jetlinks/core/SimpleValue.java
+++ src/main/java/org/jetlinks/core/SimpleValue.java
@@ -1,6 +1,7 @@
 package org.jetlinks.core;
 
 import lombok.AllArgsConstructor;
+import org.hswebframework.web.bean.FastBeanCopier;
 
 @AllArgsConstructor(staticName = "of")
 class SimpleValue implements Value {
@@ -14,7 +15,11 @@ class SimpleValue implements Value {
 
     @Override
     public <T> T as(Class<T> type) {
-        // TODO: 2019-10-19 不同类型转换支持
-        return type.cast(nativeValue);
+        if (nativeValue == null) {
+            return null;
+        }
+        return FastBeanCopier.DEFAULT_CONVERT.convert(
+                nativeValue, type, FastBeanCopier.EMPTY_CLASS_ARRAY
+        );
     }
 }
