b76ce23c84b8d580cfb17bd4ed49433ae8b4807c
zhou-hao <admin@hsweb.me>
2020-04-13 13:58:26 +0800

    增加透传消息

---
+++ src/main/java/org/jetlinks/core/message/DirectDeviceMessage.java
@@ -0,0 +1,21 @@
+package org.jetlinks.core.message;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.annotation.Nonnull;
+
+/**
+ * 透传设备消息
+ *
+ * @author zhouhao
+ * @see 1.0.2
+ */
+@Getter
+@Setter
+public class DirectDeviceMessage extends CommonDeviceMessage {
+
+    @Nonnull
+    private byte[] payload;
+
+}
