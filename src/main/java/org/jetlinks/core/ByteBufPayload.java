f742a81ce2e3681da0585f4a717b54dc1bed1a8c
zhou-hao <admin@hsweb.me>
2020-11-20 13:51:34 +0800

    使用对象池管理Payload

--- src/main/java/org/jetlinks/core/ByteBufPayload.java
+++ src/main/java/org/jetlinks/core/ByteBufPayload.java
@@ -1,27 +1,70 @@
 package org.jetlinks.core;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.util.Recycler;
 import io.netty.util.ReferenceCountUtil;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.Nonnull;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Slf4j
 class ByteBufPayload implements Payload {
 
-    private final ByteBuf body;
+    private static final Recycler<ByteBufPayload> RECYCLER = new Recycler<ByteBufPayload>() {
+        @Override
+        protected ByteBufPayload newObject(Handle<ByteBufPayload> handle) {
+            return new ByteBufPayload(handle);
+        }
+    };
+
+    private final Recycler.Handle<ByteBufPayload> handle;
+
+    ByteBufPayload(Recycler.Handle<ByteBufPayload> handle) {
+        this.handle = handle;
+    }
+
+    private ByteBuf body;
 
     public static Payload of(ByteBuf body) {
-        return new ByteBufPayload(body);
+        ByteBufPayload payload = RECYCLER.get();
+        payload.body = body;
+        return payload;
+    }
+
+    @Override
+    public boolean release() {
+        return handleRelease(ReferenceCountUtil.release(body));
+    }
+
+    @Override
+    public boolean release(int dec) {
+        return handleRelease(ReferenceCountUtil.release(body, dec));
+    }
+
+    @Override
+    public Payload retain(int inc) {
+        ReferenceCountUtil.retain(body, inc);
+        return this;
+    }
+
+    @Override
+    public Payload retain() {
+        ReferenceCountUtil.retain(body);
+        return this;
     }
 
     @Nonnull
     @Override
     public ByteBuf getBody() {
         return body;
+    }
+
+    protected boolean handleRelease(boolean release) {
+        if (release) {
+            body = null;
+            handle.recycle(this);
+        }
+        return release;
     }
 
     @Override
