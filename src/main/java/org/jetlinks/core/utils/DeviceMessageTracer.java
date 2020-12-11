e4e667aecfa09664ca86ece0e62bcfc80559d391
zhou-hao <admin@hsweb.me>
2020-12-11 12:45:40 +0800

    增加DeviceMessageTracer

---
+++ src/main/java/org/jetlinks/core/utils/DeviceMessageTracer.java
@@ -0,0 +1,18 @@
+package org.jetlinks.core.utils;
+
+import org.jetlinks.core.message.Headers;
+import org.jetlinks.core.message.Message;
+
+public class DeviceMessageTracer {
+
+    public static void trace(Message message, String name) {
+        trace(message, name, System.currentTimeMillis());
+    }
+
+    public static void trace(Message message, String name, Object value) {
+        if (message.getHeaderOrDefault(Headers.enableTrace)) {
+            message.addHeader("_trace:" + name, value);
+        }
+    }
+
+}
