20a6bc77a6dbfa40fc2150df0c723271afb78af7
zhou-hao <admin@hsweb.me>
2020-12-18 20:37:42 +0800

    doOnReBalance

--- src/main/java/org/jetlinks/core/cluster/HaManager.java
+++ src/main/java/org/jetlinks/core/cluster/HaManager.java
@@ -1,8 +1,10 @@
 package org.jetlinks.core.cluster;
 
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * 集群高可用管理器,可用于监听集群中的节点上下线信息.
@@ -32,4 +34,10 @@ public interface HaManager {
      */
     List<ServerNode> getAllNode();
 
+    /**
+     * 监听集群重新负载事件
+     *
+     * @param runnable 监听器
+     */
+    Disposable doOnReBalance(Consumer<List<ServerNode>> runnable);
 }
