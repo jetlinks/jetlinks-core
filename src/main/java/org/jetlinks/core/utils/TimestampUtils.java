5baaa2d31f0f50b8edc1fdd823190c157a5d9812
zhouhao <zh.sqy@qq.com>
2020-12-19 22:25:51 +0800

    优化时间戳长度判断

--- src/main/java/org/jetlinks/core/utils/TimestampUtils.java
+++ src/main/java/org/jetlinks/core/utils/TimestampUtils.java
@@ -3,11 +3,11 @@ package org.jetlinks.core.utils;
 public class TimestampUtils {
 
     public static long toMillis(long timestamp) {
-        return NumberUtils.fixLength(timestamp, 13);
+        return NumberUtils.fixLength(timestamp, (int) Math.log10(System.currentTimeMillis()) + 1);
     }
 
     public static long toNanos(long timestamp) {
-        return NumberUtils.fixLength(timestamp, 14);
+        return NumberUtils.fixLength(timestamp,  (int) Math.log10(System.nanoTime()) + 1);
     }
 
 
