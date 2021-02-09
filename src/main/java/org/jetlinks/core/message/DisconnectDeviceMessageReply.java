3623f0e2f03b60a96ff96cb1dc81856d01a3127b
zhouhao <zh.sqy@qq.com>
2021-02-09 16:50:17 +0800

    修复断开连接消息错误

--- src/main/java/org/jetlinks/core/message/DisconnectDeviceMessageReply.java
+++ src/main/java/org/jetlinks/core/message/DisconnectDeviceMessageReply.java
@@ -2,7 +2,7 @@ package org.jetlinks.core.message;
 
 public class DisconnectDeviceMessageReply extends CommonDeviceMessageReply<DisconnectDeviceMessageReply> {
     public MessageType getMessageType() {
-        return MessageType.DISCONNECT;
+        return MessageType.DISCONNECT_REPLY;
     }
 
 }
