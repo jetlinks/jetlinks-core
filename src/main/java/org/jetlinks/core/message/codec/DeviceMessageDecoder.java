11928e0d6c2555adf3e2e4ce3441255228d13840
zhou-hao <admin@hsweb.me>
2021-03-12 14:19:34 +0800

    优化注释

--- src/main/java/org/jetlinks/core/message/codec/DeviceMessageDecoder.java
+++ src/main/java/org/jetlinks/core/message/codec/DeviceMessageDecoder.java
@@ -5,13 +5,66 @@ import org.reactivestreams.Publisher;
 
 import javax.annotation.Nonnull;
 
+/**
+ * 设备消息解码器，用于将收到设备上传的消息解码为可读的消息。
+ *
+ * @author zhouhao
+ * @since 1.0
+ */
 public interface DeviceMessageDecoder {
+
     /**
-     * 解码，用于将收到设备上传的消息解码为可读的消息
+     * 在服务器收到设备或者网络组件中发来的消息时，会调用协议包中的此方法来进行解码，
+     * 将数据{@link EncodedMessage}转为平台的统一消息{@link org.jetlinks.core.message.DeviceMessage}
+     *
+     * <pre>
+     * //解码并返回一个消息
+     * public Mono&lt;DeviceMessage&gt; decode(MessageDecodeContext context){
+     *
+     *  EncodedMessage message = context.getMessage();
+     *  byte[] payload = message.payloadAsBytes();//上报的数据
+     *
+     *  DeviceMessage message = doEncode(payload);
+     *
+     *  return Mono.just(message);
+     * }
+     *
+     * //解码并返回多个消息
+     * public Flux&lt;DeviceMessage&gt; decode(MessageDecodeContext context){
+     *
+     *  EncodedMessage message = context.getMessage();
+     *  byte[] payload = message.payloadAsBytes();//上报的数据
+     *
+     *  List&lt;DeviceMessage&gt; messages = doEncode(payload);
+     *
+     *  return Flux.fromIterable(messages);
+     * }
+     *
+     * //解码,回复设备并返回一个消息
+     * public Mono&lt;DeviceMessage&gt; decode(MessageDecodeContext context){
+     *
+     *  EncodedMessage message = context.getMessage();
+     *  byte[] payload = message.payloadAsBytes();//上报的数据
+     *
+     *  DeviceMessage message = doEncode(payload); //解码
+     *
+     *  FromDeviceMessageContext ctx = (FromDeviceMessageContext)context;
+     *
+     *  EncodedMessage msg = createReplyMessage(); //构造回复
+     *
+     *  return ctx
+     *     .getSession()
+     *     .send(msg) //发送回复
+     *     .thenReturn(message);
+     * }
+     *
+     * </pre>
      *
      * @param context 消息上下文
      * @return 解码结果
      * @see MqttMessage
+     * @see org.jetlinks.core.message.codec.http.HttpExchangeMessage
+     * @see CoapExchangeMessage
      * @see org.jetlinks.core.message.DeviceMessageReply
      * @see org.jetlinks.core.message.property.ReadPropertyMessageReply
      * @see org.jetlinks.core.message.function.FunctionInvokeMessageReply
--- src/main/java/org/jetlinks/core/message/codec/DeviceMessageEncoder.java
+++ src/main/java/org/jetlinks/core/message/codec/DeviceMessageEncoder.java
@@ -11,9 +11,34 @@ import javax.annotation.Nonnull;
  * @see org.jetlinks.core.message.Message
  */
 public interface DeviceMessageEncoder {
+
     /**
-     * 编码,将消息进行编码,用于发送到设备端
+     * 编码,将消息进行编码,用于发送到设备端.
      *
+     * 平台在发送指令给设备时,会调用协议包中设置的此方法,将平台消息{@link org.jetlinks.core.message.DeviceMessage}转为设备能理解的消息{@link EncodedMessage}.
+     *
+     * 例如:
+     * <pre>
+     *
+     * //返回单个消息给设备,多个使用Flux&lt;EncodedMessage&gt;作为返回值
+     * public Mono&lt;EncodedMessage&gt; encode(MessageEncodeContext context){
+     *
+     *     return Mono.just(doEncode(context.getMessage()));
+     *
+     * }
+     *</pre>
+     *
+     * <pre>
+     * //忽略发送给设备,直接返回结果给指令发送者
+     * public Mono&lt;EncodedMessage&gt; encode(MessageEncodeContext context){
+     *    DeviceMessage message = (DeviceMessage)context.getMessage();
+     *
+     *    return context
+     *      .reply(handleMessage(message)) //返回结果给指令发送者
+     *      .then(Mono.empty())
+     * }
+     *
+     * </pre>
      * @param context 消息上下文
      * @return 编码结果
      * @see MqttMessage
