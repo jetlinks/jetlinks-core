147341d447379d5cc932733963da14c8f9280c7f
zhou-hao <admin@hsweb.me>
2021-04-27 18:50:20 +0800

    增加PersistentSession

---
+++ src/main/java/org/jetlinks/core/server/session/PersistentSession.java
@@ -0,0 +1,23 @@
+package org.jetlinks.core.server.session;
+
+/**
+ * 支持持久化的Session
+ *
+ * @author zhouhao
+ * @since 1.1.6
+ */
+public interface PersistentSession extends DeviceSession {
+
+    /**
+     * @return 会话提供者
+     */
+    String getProvider();
+
+    /**
+     * 序列化会话数据
+     *
+     * @return 会话数据
+     */
+    byte[] serialize();
+
+}
