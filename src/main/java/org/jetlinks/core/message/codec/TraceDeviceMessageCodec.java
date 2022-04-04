5920c5ddd038948debe463fcef890cc7db62217e
zhouhao <zh.sqy@qq.com>
2022-04-04 09:37:41 +0800

    add TraceDeviceMessageCodec

---
+++ src/main/java/org/jetlinks/core/message/codec/TraceDeviceMessageCodec.java
@@ -0,0 +1,36 @@
+package org.jetlinks.core.message.codec;
+
+import lombok.AllArgsConstructor;
+import org.jetlinks.core.message.Message;
+import org.jetlinks.core.trace.FluxTracer;
+import org.jetlinks.core.trace.ProtocolTracer;
+import reactor.core.publisher.Flux;
+
+import javax.annotation.Nonnull;
+
+@AllArgsConstructor
+public class TraceDeviceMessageCodec implements DeviceMessageCodec {
+    private final String protocolId;
+    private final DeviceMessageCodec target;
+
+    @Override
+    public Transport getSupportTransport() {
+        return target.getSupportTransport();
+    }
+
+    @Nonnull
+    @Override
+    public Flux<? extends Message> decode(@Nonnull MessageDecodeContext context) {
+        return Flux
+                .from(target.decode(context))
+                .as(FluxTracer.create(ProtocolTracer.SpanName.decode(protocolId)));
+    }
+
+    @Nonnull
+    @Override
+    public Flux<? extends EncodedMessage> encode(@Nonnull MessageEncodeContext context) {
+        return Flux
+                .from(target.encode(context))
+                .as(FluxTracer.create(ProtocolTracer.SpanName.encode(protocolId)));
+    }
+}
