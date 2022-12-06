ee8322c1c76c7af5c5538e05eeab888efa78df42
zhouhao <zh.sqy@qq.com>
2022-12-06 14:19:25 +0800

    优化http exchange

--- src/main/java/org/jetlinks/core/message/codec/http/HttpExchangeMessage.java
+++ src/main/java/org/jetlinks/core/message/codec/http/HttpExchangeMessage.java
@@ -1,13 +1,22 @@
 package org.jetlinks.core.message.codec.http;
 
+import io.netty.buffer.ByteBuf;
 import org.springframework.http.MediaType;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
 
 
 /**
- * 可响应的http消息
+ * 可响应的http消息,在{@link HttpExchangeMessage#payload()}有结果之前,
+ * {@link HttpExchangeMessage#getPayload()}
+ * 和
+ * {@link HttpExchangeMessage#multiPart()}可能为空。
+ * <p>
+ * 如果需要获取请求体,请使用{@link HttpExchangeMessage#payload()}获取.
+ * <p>
+ * 如果需要获取文件上传信息,请使用{@link HttpExchangeMessage#multiPartAsync()}获取.
  *
  * @author zhouhao
  * @see HttpRequestMessage
@@ -17,12 +26,67 @@ import javax.annotation.Nonnull;
  */
 public interface HttpExchangeMessage extends HttpRequestMessage {
 
+    /**
+     * 异步获取请求体内容. 大部分场景都需要对http请求进行合法性校验.
+     * 由于一些非法请求可能会构造比较大的数据导致内存溢出,因此需要异步获取请求体内容.
+     * <p>
+     * 先校验请求,再读取请求体.
+     *
+     * <pre>{@code
+     *   this
+     *   .validate(message)//验证请求
+     *   .then(message.payload())//读取payload
+     *   .map(this::doDecode)//解码
+     * }</pre>
+     *
+     * @return 响应消息
+     * @since 2.0
+     */
+    default Mono<ByteBuf> payload() {
+        return Mono.just(getPayload());
+    }
+
+
+    /**
+     * 异步获取文件上传内容
+     *
+     * @return 文件上传内容
+     */
+    default Mono<MultiPart> multiPartAsync() {
+        return payload()
+                .then(Mono.defer(() -> Mono.justOrEmpty(HttpRequestMessage.super.multiPart())));
+    }
+
+    /**
+     * 请使用 {@link HttpExchangeMessage#multiPartAsync()}获取
+     *
+     * @return 文件上传信息
+     */
+    @Deprecated
+    @Override
+    default Optional<MultiPart> multiPart() {
+        return HttpRequestMessage.super.multiPart();
+    }
+
+    /**
+     * 请使用 {@link  HttpExchangeMessage#payload()}方法来获取
+     *
+     * @return 已解析的请求体
+     * @deprecated 已弃用, 请使用 payload() 方法替代
+     */
+    @Nonnull
+    @Override
+    @Deprecated
+    ByteBuf getPayload();
+
+
     @Nonnull
     Mono<Void> response(@Nonnull HttpResponseMessage message);
 
     default Mono<Void> ok(@Nonnull String message) {
         return response(
-                SimpleHttpResponseMessage.builder()
+                SimpleHttpResponseMessage
+                        .builder()
                         .contentType(MediaType.APPLICATION_JSON)
                         .status(200)
                         .body(message)
@@ -32,10 +96,10 @@ public interface HttpExchangeMessage extends HttpRequestMessage {
 
     default Mono<Void> error(int status, @Nonnull String message) {
         return response(SimpleHttpResponseMessage.builder()
-                .contentType(MediaType.APPLICATION_JSON)
-                .status(status)
-                .body(message)
-                .build());
+                                                 .contentType(MediaType.APPLICATION_JSON)
+                                                 .status(status)
+                                                 .body(message)
+                                                 .build());
     }
 
     default SimpleHttpResponseMessage.SimpleHttpResponseMessageBuilder newResponse() {
