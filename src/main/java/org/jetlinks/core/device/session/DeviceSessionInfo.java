efc7e69aa7e483af6d7a78f8ef0957187ec36a23
zhouhao <zh.sqy@qq.com>
2023-04-24 13:35:36 +0800

    refactor: 设备会话信息增加parentDeviceId

--- src/main/java/org/jetlinks/core/device/session/DeviceSessionInfo.java
+++ src/main/java/org/jetlinks/core/device/session/DeviceSessionInfo.java
@@ -2,26 +2,55 @@ package org.jetlinks.core.device.session;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.jetlinks.core.server.session.ChildrenDeviceSession;
 import org.jetlinks.core.server.session.DeviceSession;
 
 import java.io.Serializable;
+import java.net.InetSocketAddress;
 
 @Getter
 @Setter
 public class DeviceSessionInfo implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 设备ID
+     */
     private String deviceId;
 
+    /**
+     * 服务节点ID
+     */
     private String serverId;
 
+    /**
+     * 设备地址
+     *
+     * @see InetSocketAddress
+     */
     private String address;
 
+    /**
+     * 连接时间
+     */
     private long connectTime;
 
+    /**
+     * 最后一次通信时间
+     */
     private long lastCommTime;
 
+    /**
+     * 通信协议
+     *
+     * @see org.jetlinks.core.message.codec.Transport
+     */
     private String transport;
+
+    /**
+     * 上级设备ID,子设备时有效
+     */
+    private String parentDeviceId;
 
     public static DeviceSessionInfo of(String serverId, DeviceSession session) {
         DeviceSessionInfo sessionInfo = new DeviceSessionInfo();
@@ -35,6 +64,11 @@ public class DeviceSessionInfo implements Serializable {
         if (null != session.getTransport()) {
             sessionInfo.setTransport(session.getTransport().getId());
         }
+        //子设备
+        if (session.isWrapFrom(ChildrenDeviceSession.class)) {
+            sessionInfo.setParentDeviceId(session.unwrap(ChildrenDeviceSession.class).getParentDevice().getDeviceId());
+        }
+
         return sessionInfo;
     }
 }
