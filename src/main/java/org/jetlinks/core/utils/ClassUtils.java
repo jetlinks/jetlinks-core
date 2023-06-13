6fa2299d81346df7f02383c55ee61a253e4fede5
zhouhao <zh.sqy@qq.com>
2023-06-13 14:34:41 +0800

    feat: 优化检查

--- src/main/java/org/jetlinks/core/utils/ClassUtils.java
+++ src/main/java/org/jetlinks/core/utils/ClassUtils.java
@@ -10,11 +10,16 @@ import org.springframework.core.type.ClassMetadata;
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.util.ResourceUtils;
+import reactor.function.Consumer3;
 import reactor.function.Function3;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.BiFunction;
 
@@ -47,39 +52,21 @@ public class ClassUtils {
                                                                             String location,
                                                                             boolean jar,
                                                                             Loader loader,
+                                                                            Consumer3<Loader,String,InputStream> walker,
                                                                             Function3<Loader, String, InputStream, Class<?>> loadFunction) {
+        return createScanner(loader, location, jar)
+                .walkClass(walker)
+                .findImplClass(superType, loadFunction);
+    }
 
-        CachingMetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory();
-        PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver(loader) {
-            @Override
-            protected boolean isJarResource(@NonNull Resource resource) {
-                return jar;
-            }
-        };
-        Resource[] classes = resourcePatternResolver.getResources(location);
-        for (Resource aClass : classes) {
-            MetadataReader reader = metadataReaderFactory.getMetadataReader(aClass);
-            AnnotationMetadata annotationMetadata = reader.getAnnotationMetadata();
-            if (annotationMetadata.hasAnnotation("java.lang.Deprecated")) {
-                continue;
-            }
-            ClassMetadata classMetadata = reader.getClassMetadata();
-            try (InputStream stream = aClass.getInputStream()) {
-                Class<?> clazz = loadFunction.apply(loader, classMetadata.getClassName(), stream);
-                if (superType.isAssignableFrom(clazz)) {
-                    return Optional.of(
-                            (T) clazz.getDeclaredConstructor().newInstance()
-                    );
-                }
-            } catch (RuntimeException e) {
-                throw e;
-            } catch (Throwable e) {
-                log.warn("load class [{}] error {}", classMetadata.getClassName(), e.getLocalizedMessage(), e);
-            }
-        }
-        metadataReaderFactory.clearCache();
 
-        return Optional.empty();
+    @SneakyThrows
+    public static <T, Loader extends ClassLoader> Optional<T> findImplClass(Class<T> superType,
+                                                                            String location,
+                                                                            boolean jar,
+                                                                            Loader loader,
+                                                                            Function3<Loader, String, InputStream, Class<?>> loadFunction) {
+        return createScanner(loader, location, jar).findImplClass(superType, loadFunction);
     }
 
     public static <T, Loader extends ClassLoader> Optional<T> findImplClass(Class<T> superType,
@@ -90,4 +77,71 @@ public class ClassUtils {
         return findImplClass(superType, location, jar, loader, (_loader, classname, stream) -> loadFunction.apply(_loader, classname));
     }
 
+    public static <Loader extends ClassLoader> Scanner<Loader> createScanner(Loader loader,
+                                                                             String location,
+                                                                             boolean jar) {
+        return new Scanner<>(loader, location, jar);
+    }
+
+
+    public static class Scanner<Loader extends ClassLoader> {
+
+        private final Loader loader;
+
+        private final List<MetadataReader> classResources;
+
+        @SneakyThrows
+        public Scanner(Loader loader, String location, boolean jar) {
+            this.loader = loader;
+            CachingMetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory();
+            PathMatchingResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver(loader) {
+                @Override
+                protected boolean isJarResource(@NonNull Resource resource) {
+                    return jar;
+                }
+            };
+            Resource[] classes = resourcePatternResolver.getResources(location);
+            classResources = new ArrayList<>(classes.length);
+            for (Resource aClass : classes) {
+                classResources.add(metadataReaderFactory.getMetadataReader(aClass));
+            }
+            metadataReaderFactory.clearCache();
+
+        }
+
+        @SneakyThrows
+        public Scanner<Loader> walkClass(Consumer3<Loader, String, InputStream> consumer) {
+            for (MetadataReader reader : classResources) {
+                try (InputStream stream = reader.getResource().getInputStream()) {
+                    consumer.accept(loader, reader.getClassMetadata().getClassName(), stream);
+                }
+            }
+            return this;
+        }
+
+        public <T> Optional<T> findImplClass(Class<T> superType,
+                                             Function3<Loader, String, InputStream, Class<?>> loader) {
+            for (MetadataReader reader : classResources) {
+                AnnotationMetadata annotationMetadata = reader.getAnnotationMetadata();
+                if (annotationMetadata.hasAnnotation("java.lang.Deprecated")) {
+                    continue;
+                }
+                ClassMetadata classMetadata = reader.getClassMetadata();
+                try (InputStream stream = reader.getResource().getInputStream()) {
+                    Class<?> clazz = loader.apply(this.loader, classMetadata.getClassName(), stream);
+                    if (superType.isAssignableFrom(clazz)) {
+                        return Optional.of(
+                                (T) clazz.getDeclaredConstructor().newInstance()
+                        );
+                    }
+                } catch (RuntimeException e) {
+                    throw e;
+                } catch (Throwable e) {
+                    log.warn("load class [{}] error {}", classMetadata.getClassName(), e.getLocalizedMessage(), e);
+                }
+            }
+            return Optional.empty();
+        }
+
+    }
 }
