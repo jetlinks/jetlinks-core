5187581712d857b6eb9472b2eade99272a7e2a51
zhouhao <zh.sqy@qq.com>
2023-08-03 14:52:53 +0800

    refactor: 缓存增加共享功能,减少常驻内存占用.

--- src/main/java/org/jetlinks/core/SimpleValue.java
+++ src/main/java/org/jetlinks/core/SimpleValue.java
@@ -1,10 +1,12 @@
 package org.jetlinks.core;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
 
 @AllArgsConstructor(staticName = "of")
+@EqualsAndHashCode
 class SimpleValue implements Value, Serializable {
 
     private final Object nativeValue;
