8979dd0b0bfd160bc8d3c4f5d4e4adf954c6cf22
zhouhao <zh.sqy@qq.com>
2023-09-14 18:38:34 +0800

    feat: SimpleObjectProperty实现序列化接口

--- src/main/java/org/jetlinks/core/things/relation/SimpleObjectProperty.java
+++ src/main/java/org/jetlinks/core/things/relation/SimpleObjectProperty.java
@@ -4,12 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.jetlinks.core.utils.SerializeUtils;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
 @Getter
 @Setter
 @AllArgsConstructor(staticName = "of")
 @NoArgsConstructor
-public class SimpleObjectProperty implements ObjectProperty {
+public class SimpleObjectProperty implements ObjectProperty, Externalizable {
     private String property;
     private Object value;
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeUTF(property);
+        SerializeUtils.writeObject(value, out);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        property = in.readUTF();
+        value = SerializeUtils.readObject(in);
+    }
 }
