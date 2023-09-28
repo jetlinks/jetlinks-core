34574a1a6e3864d607b43a32338a715f1dd72f29
Zhang Ji <125540670@qq.com>
2023-09-28 14:37:17 +0800

    fix(设备消息): 解决属性格式化精度不正确的问题 (#15)

--- src/main/java/org/jetlinks/core/metadata/types/NumberType.java
+++ src/main/java/org/jetlinks/core/metadata/types/NumberType.java
@@ -68,7 +68,7 @@ public abstract class NumberType<N extends Number> extends AbstractType<NumberTy
             return null;
         }
         Number val = convertScaleNumber(value,
-                                        this.getScale(),
+                                        this.getScale(defaultScale()),
                                         getRound(),
                                         Function.identity());
         if (val == null) {
