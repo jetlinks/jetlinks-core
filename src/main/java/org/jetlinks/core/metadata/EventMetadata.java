a153ba39e1637d4cc1156cd885a07c660611af2d
zhangji <125540670@qq.com>
2023-10-17 10:38:13 +0800

    fix(物模型): 优化物模型事件的兼容性

--- src/main/java/org/jetlinks/core/metadata/EventMetadata.java
+++ src/main/java/org/jetlinks/core/metadata/EventMetadata.java
@@ -9,6 +9,10 @@ public interface EventMetadata extends Metadata ,Jsonable{
 
     DataType getType();
 
+    default DataType getValueType() {
+        return getType();
+    }
+
     default EventMetadata merge(EventMetadata another, MergeOption... option){
         throw new UnsupportedOperationException("不支持事件物模型合并");
     }
