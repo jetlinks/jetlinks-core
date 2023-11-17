52c095fe6c080c9bb82b1ec1d34d1e5a8f5fb52f
zhouhao <zh.sqy@qq.com>
2023-11-17 18:10:27 +0800

    fix: 修复DerivedMetadataMessage序列化错误问题

--- src/main/java/org/jetlinks/core/message/DerivedMetadataMessage.java
+++ src/main/java/org/jetlinks/core/message/DerivedMetadataMessage.java
@@ -2,6 +2,11 @@ package org.jetlinks.core.message;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.jetlinks.core.utils.SerializeUtils;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
 @Getter
 @Setter
@@ -16,5 +21,19 @@ public class DerivedMetadataMessage extends CommonDeviceMessage<DerivedMetadataM
     @Override
     public MessageType getMessageType() {
         return MessageType.DERIVED_METADATA;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        super.writeExternal(out);
+        SerializeUtils.writeNullableUTF(metadata,out);
+        out.writeBoolean(all);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+        metadata = SerializeUtils.readNullableUTF(in);
+        all = in.readBoolean();
     }
 }
