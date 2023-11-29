9bdc01627e4e8c114e1f7dfa168ed9ff644a16c5
老周 <zh.sqy@qq.com>
2023-11-29 14:35:03 +0800

    fix: 修复ReactiveCacheContainer递归调用时无响应问题 (#19)

--- src/test/java/org/jetlinks/core/cache/ReactiveCacheContainerTest.java
+++ src/test/java/org/jetlinks/core/cache/ReactiveCacheContainerTest.java
@@ -3,6 +3,7 @@ package org.jetlinks.core.cache;
 import org.junit.Test;
 import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
@@ -12,30 +13,50 @@ import static org.junit.Assert.*;
 
 public class ReactiveCacheContainerTest {
 
+
+    @Test
+    public void testNest() {
+        ReactiveCacheContainer<String, String> cache = ReactiveCacheContainer.create();
+
+        cache.compute("test", (k, v) -> {
+                 return Mono.defer(() -> cache
+                                .get("test", Mono.just("x"))
+                                .doOnNext(System.out::println))
+                            .thenReturn("1");
+             })
+             //.subscribeOn(Schedulers.parallel())
+             .as(StepVerifier::create)
+             .expectNext("1")
+             .verifyComplete();
+
+    }
+
     @Test
     public void testError() {
-        ReactiveCacheContainer<String,Disposable> cache = ReactiveCacheContainer.create();
+        ReactiveCacheContainer<String, Disposable> cache = ReactiveCacheContainer.create();
         cache
-                .compute("test", (k, v) -> Mono
-                        .error(new RuntimeException("error"))
-                        .thenReturn(() -> {}))
-                .as(StepVerifier::create)
-                .expectError()
-                .verify();
+            .compute("test", (k, v) -> Mono
+                .error(new RuntimeException("error"))
+                .thenReturn(() -> {
+                }))
+            .as(StepVerifier::create)
+            .expectError()
+            .verify();
 
-        assertNull(cache.get("test",null));
+        assertNull(cache.get("test", null));
     }
+
     @Test
-   public void test() {
-        ReactiveCacheContainer<String,Disposable> cache = ReactiveCacheContainer.create();
+    public void test() {
+        ReactiveCacheContainer<String, Disposable> cache = ReactiveCacheContainer.create();
         AtomicBoolean disposed = new AtomicBoolean();
         cache
-                .compute("test", (k, v) -> Mono
-                        .delay(Duration.ofMillis(100))
-                        .thenReturn(() -> disposed.set(true)))
-                .as(StepVerifier::create)
-                .expectNextCount(1)
-                .verifyComplete();
+            .compute("test", (k, v) -> Mono
+                .delay(Duration.ofMillis(100))
+                .thenReturn(() -> disposed.set(true)))
+            .as(StepVerifier::create)
+            .expectNextCount(1)
+            .verifyComplete();
 
         cache.remove("test");
 
