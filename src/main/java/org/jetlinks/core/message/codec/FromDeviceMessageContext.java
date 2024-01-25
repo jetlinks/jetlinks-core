2d5ea8c07d00dc260b4ac408857a5b31dc0f5566
zhouhao <zh.sqy@qq.com>
2024-01-25 15:12:51 +0800

    feat: MessageDecodeContext增加handleMessage方法.

--- src/main/java/org/jetlinks/core/message/codec/FromDeviceMessageContext.java
+++ src/main/java/org/jetlinks/core/message/codec/FromDeviceMessageContext.java
@@ -3,10 +3,12 @@ package org.jetlinks.core.message.codec;
 
 import org.jetlinks.core.device.DeviceOperator;
 import org.jetlinks.core.device.DeviceRegistry;
+import org.jetlinks.core.message.DeviceMessage;
 import org.jetlinks.core.server.session.DeviceSession;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Nonnull;
+import java.util.function.Function;
 
 /**
  * 来自设备的消息上下文，可以通过此上下文获取设备会话
@@ -56,6 +58,34 @@ public interface FromDeviceMessageContext extends MessageDecodeContext {
             @Override
             public Mono<DeviceOperator> getDevice(String deviceId) {
                 return registry.getDevice(deviceId);
+            }
+        };
+    }
+
+    static FromDeviceMessageContext of(DeviceSession session,
+                                       EncodedMessage message,
+                                       DeviceRegistry registry,
+                                       Function<DeviceMessage,Mono<Void>> handler) {
+        return new FromDeviceMessageContext() {
+            @Override
+            public DeviceSession getSession() {
+                return session;
+            }
+
+            @Nonnull
+            @Override
+            public EncodedMessage getMessage() {
+                return message;
+            }
+
+            @Override
+            public Mono<DeviceOperator> getDevice(String deviceId) {
+                return registry.getDevice(deviceId);
+            }
+
+            @Override
+            public Mono<Void> handleMessage(DeviceMessage message) {
+                return handler.apply(message);
             }
         };
     }
--- src/main/java/org/jetlinks/core/message/codec/MessageDecodeContext.java
+++ src/main/java/org/jetlinks/core/message/codec/MessageDecodeContext.java
@@ -1,7 +1,12 @@
 package org.jetlinks.core.message.codec;
 
 
+import org.jetlinks.core.message.DeviceMessage;
+import reactor.core.publisher.Mono;
+
 import javax.annotation.Nonnull;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * @author zhouhao
@@ -24,5 +29,31 @@ public interface MessageDecodeContext extends MessageCodecContext {
      */
     @Nonnull
     EncodedMessage getMessage();
+
+    /**
+     * 手动调用处理设备消息,此操作可以感知到设备消息的处理结果. 可通过{@link Mono#onErrorResume(Function)}来处理错误.
+     * <pre>{@code
+     *
+     *   DeviceMessage msg = doDecode(context);
+     *
+     *   return context
+     *          .handleMessage(msg)
+     *          //应答错误
+     *          .onErrorResume(err-> return ackError(context).then(Mono.error(err)))
+     *          //应答成功
+     *          .then(ackSuccess(context))
+     *          //返回空,因为handleMessage已经手动处理过了
+     *          .then(Mono.empty())
+     *
+     * }
+     * @param message 设备消息
+     * @see Mono#onErrorResume(Function)
+     * @see Mono#doOnError(Consumer)
+     * @return void
+     * @since 1.2.2
+     */
+    default Mono<Void> handleMessage(DeviceMessage message) {
+        return Mono.error(new UnsupportedOperationException("handleMessage not supported"));
+    }
 
 }
