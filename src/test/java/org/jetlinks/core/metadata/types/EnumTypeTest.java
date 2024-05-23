efdc9ed0829fe9b8b764c22378b82181b28c6b29
gyl <1554863190@qq.com>
2024-05-23 19:48:42 +0800

    fix: 修正枚举类型多选时的校验及格式化逻辑

--- src/test/java/org/jetlinks/core/metadata/types/EnumTypeTest.java
+++ src/test/java/org/jetlinks/core/metadata/types/EnumTypeTest.java
@@ -1,7 +1,10 @@
 package org.jetlinks.core.metadata.types;
 
+import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -20,6 +23,25 @@ public class EnumTypeTest {
 
         Assert.assertTrue(type.validate("1").isSuccess());
         Assert.assertEquals(type.format("1"),"男");
+
+
+    }
+
+    @Test
+    public void testMulti(){
+        EnumType type=new EnumType();
+        type.setMulti(true);
+
+        type.addElement(EnumType.Element.of("1","男"));
+        type.addElement(EnumType.Element.of("2","女"));
+
+        Assert.assertTrue(type.validate("1,2").isSuccess());
+        Assert.assertEquals(type.format("1,2"), Lists.newArrayList("男","女"));
+
+        Assert.assertFalse(type.validate("3").isSuccess());
+        Assert.assertTrue(type.validate("1,3").isSuccess());
+        Assert.assertFalse(type.validate("3,4").isSuccess());
+        Assert.assertEquals(type.format("1,3"), Lists.newArrayList("男","3"));
 
 
     }
