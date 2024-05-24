37862990645453b4c7decdfaf37071cbafe24639
gyl <80454341+goten7@users.noreply.github.com>
2024-05-24 16:56:16 +0800

    fix: 修正枚举类型多选时的校验及格式化逻辑 (#26)
    * fix: 修正枚举类型多选时的校验及格式化逻辑
    
    * fix: 优化对应类型格式化

--- src/main/java/org/jetlinks/core/metadata/types/EnumType.java
+++ src/main/java/org/jetlinks/core/metadata/types/EnumType.java
@@ -10,6 +10,7 @@ import org.jetlinks.core.metadata.DataType;
 import org.jetlinks.core.metadata.ValidateResult;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -45,6 +46,19 @@ public class EnumType extends AbstractType<EnumType> implements DataType {
         if (elements == null) {
             return ValidateResult.fail("值[" + value + "]不在枚举中");
         }
+        Object _value;
+        if (multi) {
+            _value = convertMulti(value);
+        } else {
+            _value = convert(value);
+        }
+        if (_value == null) {
+            return ValidateResult.fail("值[" + value + "]不在枚举中");
+        }
+        return ValidateResult.success(_value);
+    }
+
+    private Object convert(Object value) {
         for (Element ele : elements) {
             if (match(value, ele)) {
                 //类型完全相同,则使用原始值作为对象.
@@ -52,10 +66,39 @@ public class EnumType extends AbstractType<EnumType> implements DataType {
                 if (valueType instanceof Converter) {
                     actValue = ((Converter<?>) valueType).convert(actValue);
                 }
-                return ValidateResult.success(actValue);
+                return actValue;
             }
         }
-        return ValidateResult.fail("值[" + value + "]不在枚举中");
+        return null;
+    }
+
+    public Object convertMulti(Object value) {
+        List<Object> _value = new ArrayList<>();
+        List<Object> objects = toArray(value);
+        if (!objects.isEmpty()) {
+            for (Object object : objects) {
+                Object _v = convert(object);
+                if (_v != null) {
+                    _value.add(_v);
+                }
+            }
+        }
+        if (_value.isEmpty()) {
+            return null;
+        }
+        return _value;
+    }
+
+    private static List<Object> toArray(Object value) {
+        List<Object> values = new ArrayList<>();
+        if (value instanceof Collection) {
+            values = new ArrayList<>(((Collection<?>) value));
+        }
+        if (value instanceof String) {
+            String _string = (String) value;
+            values = Arrays.asList((_string).split(","));
+        }
+        return values;
     }
 
     private boolean match(Object value, Element ele) {
@@ -71,17 +114,30 @@ public class EnumType extends AbstractType<EnumType> implements DataType {
     }
 
     @Override
-    public String format(Object value) {
-        String stringVal = String.valueOf(value);
+    public Object format(Object value) {
         if (elements == null) {
-            return stringVal;
+            return String.valueOf(value);
         }
+        if (multi) {
+            List<String> _format = toArray(value)
+                    .stream()
+                    .map(this::format0)
+                    .collect(Collectors.toList());
+            if (value instanceof String) {
+                return String.join(",", _format);
+            }
+            return _format;
+        }
+        return format0(value);
+    }
+
+    private String format0(Object value) {
         return elements
             .stream()
             .filter(ele -> String.valueOf(value).equals(ele.value))
             .findFirst()
             .map(Element::getText)
-            .orElse(stringVal);
+            .orElse(String.valueOf(value));
     }
 
     public EnumType addElement(Element element) {
