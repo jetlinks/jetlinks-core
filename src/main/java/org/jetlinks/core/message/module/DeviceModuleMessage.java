55c661f27a7b9c1286c0948b9a2f4ac50849c680
zhouhao <zh.sqy@qq.com>
2024-07-11 16:26:03 +0800

    refactor: 增加物模型模块相关功能拓展定义

---
+++ src/main/java/org/jetlinks/core/message/module/DeviceModuleMessage.java
@@ -0,0 +1,47 @@
+package org.jetlinks.core.message.module;
+
+import org.jetlinks.core.message.CommonDeviceMessage;
+import org.jetlinks.core.message.CommonThingMessage;
+import org.jetlinks.core.message.Message;
+import org.jetlinks.core.message.MessageType;
+
+/**
+ * 设备模块消息
+ *
+ * @author zhouhao
+ * @since 1.2.2
+ * @see ThingModuleMessage
+ */
+public class DeviceModuleMessage extends CommonDeviceMessage<DeviceModuleMessage>
+    implements ThingModuleMessage {
+
+    private String module;
+    private Message message;
+
+    @Override
+    public final MessageType getMessageType() {
+        return MessageType.MODULE;
+    }
+
+    @Override
+    public String getModule() {
+        return module;
+    }
+
+    @Override
+    public DeviceModuleMessage module(String module) {
+        this.module = module;
+        return this;
+    }
+
+    @Override
+    public Message getMessage() {
+        return message;
+    }
+
+    @Override
+    public DeviceModuleMessage message(Message message) {
+        this.message = message;
+        return this;
+    }
+}
