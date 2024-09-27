c655891cb0438b6ad4cd198aa2611602a2a5f42d
liusq <441032531@qq.com>
2024-09-27 14:47:22 +0800

    feat(jetlinks-core): BindInfo实现Externalizable接口

--- src/main/java/org/jetlinks/core/device/manager/BindInfo.java
+++ src/main/java/org/jetlinks/core/device/manager/BindInfo.java
@@ -5,12 +5,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
 @Getter
 @Setter
 @AllArgsConstructor(staticName = "of")
 @NoArgsConstructor
-public class BindInfo {
+public class BindInfo implements Externalizable {
     private String key;
     private String deviceId;
     private String description;
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeUTF(key);
+        out.writeUTF(deviceId);
+        out.writeUTF(description);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        this.key = in.readUTF();
+        this.deviceId = in.readUTF();
+        this.description = in.readUTF();
+    }
 }
