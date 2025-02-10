f7ca686571f3ce8941d5f71ed9b40efa85f13701
fighter-wang <118291973+fighter-wang@users.noreply.github.com>
2025-02-10 15:52:09 +0800

    fix(核心模块): 修复ArrayType类型中的转换方法的传参实际类型可能为数组而非集合，导致的数据格式转换错误问题 (#50)
    * fix(核心模块): 修复ArrayType类型中的转换方法的传参实际类型可能为数组而非集合，导致的数据格式转换错误问题
    
    * fix(核心模块): 补充ArrayType转化方法单元测试
    
    * fix(核心模块): 修复基本类型数组可能转换失败的问题
    
    ---------
    
    Co-authored-by: fighter-wang <11291691+fighter-wang@user.noreply.gitee.com>

--- src/main/java/org/jetlinks/core/metadata/types/ArrayType.java
+++ src/main/java/org/jetlinks/core/metadata/types/ArrayType.java
@@ -8,6 +8,7 @@ import org.hswebframework.web.i18n.LocaleUtils;
 import org.jetlinks.core.metadata.Converter;
 import org.jetlinks.core.metadata.DataType;
 import org.jetlinks.core.metadata.ValidateResult;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -67,6 +68,20 @@ public class ArrayType extends AbstractType<ArrayType> implements DataType, Conv
 
     @Override
     public List<Object> convert(Object value) {
+        // 判断是否为数组
+        if (value.getClass().isArray()) {
+            // 将数组转换为 List
+            return CollectionUtils
+                .arrayToList(value)
+                .stream()
+                .map(val -> {
+                    if (elementType instanceof Converter) {
+                        return ((Converter<?>) elementType).convert(val);
+                    }
+                    return val;
+                }).collect(Collectors.toList());
+        }
+        
         if (value instanceof Collection) {
             return ((Collection<?>) value).stream()
                     .map(val -> {
@@ -76,6 +91,7 @@ public class ArrayType extends AbstractType<ArrayType> implements DataType, Conv
                         return val;
                     }).collect(Collectors.toList());
         }
+        
         if(value instanceof String){
             return JSON.parseArray(String.valueOf(value));
         }
