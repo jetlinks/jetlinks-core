d652242026e784309b0ed0a29996efc182102d34
zhouhao <zh.sqy@qq.com>
2025-05-11 14:00:11 +0800

    feat(基础模块): 优化注释

--- src/main/java/org/jetlinks/core/command/CommandContext.java
+++ src/main/java/org/jetlinks/core/command/CommandContext.java
@@ -4,16 +4,58 @@ import reactor.core.publisher.Mono;
 import reactor.util.context.Context;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * 命令上下文,用于在上下文中提供命令支持,用于执行业务回调等场景.
  * <p>
  * 注意: 上下文仅会存在于同一个响应式调用中,请勿缓存上下文进行调用.
  *
+ * <pre>{@code
+ *
+ *   // 服务1
+ *   Service1{
+ *
+ *       Mono<Void> handleConnection(NetConnection connection){
+ *
+ *          // 构造上下文
+ *          CommandContext ctx = createCommand("net_comm",connection);
+ *
+ *          // 获取命令服务
+ *          CommandSupport service= ...;
+ *
+ *          return service
+ *                .execute(new HandleConnectionCommand())
+ *                .contextWrite(ctx);
+ *       }
+ *
+ *   }
+ *
+ *  // 服务2
+ *   Service2{
+ *
+ *       @CommandHandler
+ *       Mono<Void> handleCommand(HandleConnectionCommand cmd){
+ *
+ *        return this
+ *             .handleConnection(cmd)
+ *             .flatMap(ackData->{
+ *               return CommandContext
+ *                    .current("net_comm")// 获取上下文中指定的命令支持
+ *                    // 执行上下文中的命令,
+ *                    .flatMap(support->support
+ *                          .execute(new SendCommand().payload(ackData)))
+ *            })
+ *
+ *       }
+ *
+ *   }
+ * }</pre>
+ *
  * @author zhouhao
  * @since 1.3
  */
-public interface CommandContext {
+public interface CommandContext extends Function<Context, Context> {
 
     /**
      * 获取命令支持,获取不到时返回{@link Mono#empty()}
@@ -23,6 +65,15 @@ public interface CommandContext {
      * @see CommandSupport
      */
     Mono<CommandSupport> getCommandSupport(String name);
+
+    /**
+     * @see Mono#contextWrite(Function)
+     * @see reactor.core.publisher.Flux#contextWrite(Function)
+     */
+    @Override
+    default Context apply(Context context) {
+        return writeToContext(this, context);
+    }
 
     /**
      * 组合另外一个上下文,当前上下文获取不到命令时,使用另外一个上下文进行获取
@@ -35,7 +86,14 @@ public interface CommandContext {
             .switchIfEmpty(context.getCommandSupport(name));
     }
 
-    static Context writeToContext(Context context, CommandContext ctx) {
+    /**
+     * 将指定的命令上下文设置到响应式上下文并返回新的响应式上下文，不会影响传入的原始响应式上下文。
+     *
+     * @param context Context
+     * @param ctx     CommandContext
+     * @return Context
+     */
+    static Context writeToContext(CommandContext ctx, Context context) {
         CommandContext exist = context.getOrDefault(CommandContext.class, ctx);
         if (exist != null) {
             ctx = ctx.or(exist);
@@ -43,11 +101,23 @@ public interface CommandContext {
         return context.put(CommandContext.class, ctx);
     }
 
+    /**
+     * 从响应式上下文中获取命令上下文
+     *
+     * @param context 上下文
+     * @return Optional
+     */
     static Optional<CommandContext> readFromContext(Context context) {
         return context.getOrEmpty(CommandContext.class);
     }
 
-    static Mono<CommandSupport> currentCommandSupport(String name) {
+    /**
+     * 从当前响应式上下文中获取指定名称的命令支持,如果没有上下文将返回{@link Mono#empty()}.
+     *
+     * @param name 命令支持
+     * @return Optional
+     */
+    static Mono<CommandSupport> current(String name) {
         return Mono
             .deferContextual(
                 ctx -> {
@@ -59,6 +129,11 @@ public interface CommandContext {
                 });
     }
 
+    /**
+     * 从当前响应式上下文中获取上下文支持,如果没有上下文将返回{@link Mono#empty()}.
+     *
+     * @return Mono
+     */
     static Mono<CommandContext> current() {
         return Mono
             .deferContextual(
