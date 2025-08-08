72f1885b88b33cc4d971395f010d45a4a93bc88d
zhouhao <zh.sqy@qq.com>
2025-08-08 14:59:21 +0800

    refactor: 优化TopicPayload
    Signed-off-by: zhouhao <zh.sqy@qq.com>

--- src/main/java/org/jetlinks/core/event/TopicPayload.java
+++ src/main/java/org/jetlinks/core/event/TopicPayload.java
@@ -2,33 +2,34 @@ package org.jetlinks.core.event;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import io.netty.util.ReferenceCountUtil;
+import io.netty.buffer.ByteBuf;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import org.hswebframework.web.bean.FastBeanCopier;
+import org.jetlinks.core.NativePayload;
 import org.jetlinks.core.Payload;
 import org.jetlinks.core.Routable;
+import org.jetlinks.core.codec.Decoder;
 import org.jetlinks.core.message.Headers;
-import org.jetlinks.core.metadata.Jsonable;
 import org.jetlinks.core.utils.SerializeUtils;
 import org.jetlinks.core.utils.TopicUtils;
 
+import javax.annotation.Nonnull;
 import java.io.Externalizable;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
-import java.util.*;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Getter
 @AllArgsConstructor(staticName = "of")
 @Slf4j
-public class TopicPayload implements Routable, Externalizable {
+public class TopicPayload implements Payload, Routable, Externalizable {
 
     private CharSequence topic;
 
-    private Object payload;
+    private Payload payload;
 
     private Map<String, Object> headers;
 
@@ -42,16 +43,10 @@ public class TopicPayload implements Routable, Externalizable {
         return topic;
     }
 
-    public static TopicPayload of(CharSequence topic, Object payload) {
-        return TopicPayload.of(topic, payload, null);
-    }
-
-    @Deprecated
     public static TopicPayload of(CharSequence topic, Payload payload) {
         return TopicPayload.of(topic, payload, null);
     }
 
-    @Deprecated
     public static TopicPayload of(String topic, Payload payload) {
         return TopicPayload.of(topic, payload, null);
     }
@@ -81,9 +76,63 @@ public class TopicPayload implements Routable, Externalizable {
         return headers == null ? null : headers.get(key);
     }
 
-    @Deprecated
+    @Nonnull
+    @Override
+    public ByteBuf getBody() {
+        return payload.getBody();
+    }
+
+    @Override
+    public TopicPayload slice() {
+        return TopicPayload.of(topic, payload.slice());
+    }
+
+    @Override
     public boolean release() {
         return true;
+    }
+
+    @Override
+    public boolean release(int dec) {
+        return true;
+    }
+
+    protected boolean handleRelease(boolean success) {
+
+        return success;
+    }
+
+    protected void deallocate() {
+
+    }
+
+    @Override
+    public TopicPayload retain() {
+
+        return this;
+    }
+
+    @Override
+    public TopicPayload retain(int inc) {
+
+        return this;
+    }
+
+    @Override
+    public TopicPayload touch(Object o) {
+
+        return this;
+    }
+
+    @Override
+    public TopicPayload touch() {
+
+        return this;
+    }
+
+    @Override
+    public int refCnt() {
+        return 0;
     }
 
     @Override
@@ -94,97 +143,55 @@ public class TopicPayload implements Routable, Externalizable {
             '}';
     }
 
-    public JSONObject bodyToJson() {
-        return bodyToJson(true);
-    }
-
-    @SuppressWarnings("all")
+    @Override
     public JSONObject bodyToJson(boolean release) {
-        try {
-            if (payload == null) {
-                return new JSONObject();
-            }
-            if (payload instanceof Jsonable) {
-                return ((Jsonable) payload).toJson();
-            }
-            if (payload instanceof JSONObject) {
-                return ((JSONObject) payload);
-            }
-            if (payload instanceof Map) {
-                return new JSONObject(((Map) payload));
-            }
-            return FastBeanCopier.copy(payload, JSONObject::new);
-        } finally {
-            if (release) {
-                ReferenceCountUtil.safeRelease(this);
-            }
-        }
+        return payload.bodyToJson(release);
     }
 
-
-    @SuppressWarnings("all")
+    @Override
     public JSONArray bodyToJsonArray(boolean release) {
-        try {
-            if (payload == null) {
-                return new JSONArray();
-            }
-            if (payload instanceof JSONArray) {
-                return ((JSONArray) payload);
-            }
-            List<Object> collection;
-            if (payload instanceof List) {
-                collection = ((List<Object>) payload);
-            } else if (payload instanceof Collection) {
-                collection = new ArrayList<>(((Collection<Object>) payload));
-            } else if (payload instanceof Object[]) {
-                collection = Arrays.asList(((Object[]) payload));
-            } else {
-                throw new UnsupportedOperationException("body is not array");
-            }
-            return new JSONArray(collection);
-        } finally {
-            if (release) {
-                ReferenceCountUtil.safeRelease(this);
-            }
-        }
+        return payload.bodyToJsonArray(release);
     }
 
+    @Override
     public String bodyToString() {
-        return String.valueOf(payload);
+        return payload.bodyToString();
     }
 
+    @Override
     public String bodyToString(boolean release) {
-        return bodyToString();
+        return payload.bodyToString(release);
     }
 
+    @Override
     public Object decode() {
-        return payload;
+        return payload.decode();
     }
 
-
+    @Override
     public Object decode(boolean release) {
-        return decode();
+        return payload.decode(release);
     }
 
-
-    @SuppressWarnings("all")
-    public <T> T decode(Class<T> type) {
-        if (type.isInstance(payload)) {
-            return (T) payload;
-        }
-        if (type == JSONObject.class || type == Map.class) {
-            return (T) bodyToJson();
-        }
-        if (Map.class.isAssignableFrom(type)) {
-            return bodyToJson().toJavaObject(type);
-        }
-        return FastBeanCopier.copy(payload, type);
+    @Override
+    public <T> T decode(Class<T> decoder) {
+        return payload.decode(decoder);
     }
 
+    @Override
     public <T> T decode(Class<T> decoder, boolean release) {
-        return decode(decoder);
+        return payload.decode(decoder, release);
     }
 
+    @Override
+    public <T> T decode(Decoder<T> decoder) {
+        return payload.decode(decoder);
+    }
+
+    @Override
+    public <T> T decode(Decoder<T> decoder, boolean release) {
+        return payload.decode(decoder, release);
+    }
 
     public Map<String, String> getTopicVars(String pattern) {
         return TopicUtils.getPathVariables(pattern, getTopic());
@@ -193,31 +200,37 @@ public class TopicPayload implements Routable, Externalizable {
     @Override
     @SuppressWarnings("all")
     public long hash(Object... objects) {
-        if (payload instanceof Routable) {
-            return ((Routable) payload).hash(objects);
+        if (payload instanceof NativePayload) {
+            if (((NativePayload<?>) payload).getNativeObject() instanceof Routable) {
+                return ((Routable) ((NativePayload<?>) payload).getNativeObject()).hash(objects);
+            }
         }
         return Routable.super.hash(objects);
     }
 
     @SuppressWarnings("all")
     public void copyRouteKeyToHeader() {
-        if (payload instanceof Routable) {
-            addHeader(Headers.routeKey.getKey(), ((Routable) payload).routeKey());
+        if (payload instanceof NativePayload) {
+            if (((NativePayload<?>) payload).getNativeObject() instanceof Routable) {
+                addHeader(Headers.routeKey.getKey(), ((Routable) ((NativePayload<?>) payload).getNativeObject()).routeKey());
+            }
         }
     }
 
     @SuppressWarnings("all")
     @Override
     public Object routeKey() {
-        if (payload instanceof Routable) {
-            return ((Routable) payload).routeKey();
+        if (payload instanceof NativePayload) {
+            if (((NativePayload<?>) payload).getNativeObject() instanceof Routable) {
+                return ((Routable) ((NativePayload<?>) payload).getNativeObject()).routeKey();
+            }
         }
         return headers.get(Headers.routeKey.getKey());
     }
 
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
-        SerializeUtils.writeObject(topic,out);
+        SerializeUtils.writeObject(topic, out);
         SerializeUtils.writeObject(payload,out);
         SerializeUtils.writeKeyValue(headers,out);
     }
