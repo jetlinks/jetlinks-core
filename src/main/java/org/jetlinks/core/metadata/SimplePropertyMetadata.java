b50a771f3b4687b963254febf3d74da7f5013bd3
gyl <1554863190@qq.com>
2025-08-15 10:46:24 +0800

    feat: 优化

--- src/main/java/org/jetlinks/core/metadata/SimplePropertyMetadata.java
+++ src/main/java/org/jetlinks/core/metadata/SimplePropertyMetadata.java
@@ -28,6 +28,16 @@ public class SimplePropertyMetadata implements PropertyMetadata {
 
     private Map<String, Object> expands;
 
+    public static SimplePropertyMetadata of(PropertyMetadata metadata) {
+        SimplePropertyMetadata simple = new SimplePropertyMetadata();
+        simple.setId(metadata.getId());
+        simple.setName(metadata.getName());
+        simple.setDescription(metadata.getDescription());
+        simple.setExpands(metadata.getExpands());
+        simple.setValueType(metadata.getValueType());
+        return simple;
+    }
+
     public static SimplePropertyMetadata of(String id, String name, DataType type) {
         SimplePropertyMetadata metadata = new SimplePropertyMetadata();
         metadata.setId(id);
