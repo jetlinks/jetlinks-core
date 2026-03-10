69b608de9967f76a8a0905e5adc66c2fc409b6b7
zhouhao <zh.sqy@qq.com>
2026-03-10 16:02:36 +0800

    feat(消息解析): 为MessageParserFactory新增注释, 提高代码可读性

--- src/main/java/org/jetlinks/core/message/codec/MessageParserFactory.java
+++ src/main/java/org/jetlinks/core/message/codec/MessageParserFactory.java
@@ -3,7 +3,21 @@ package org.jetlinks.core.message.codec;
 import org.jetlinks.core.server.ClientConnection;
 import reactor.core.publisher.Mono;
 
+/**
+ * 消息解析起工厂,用于根据连接信息创建消息解析器,解析器用来识别和解析消息,处理粘拆包等.
+ *
+ * @author zhouhao
+ * @since 1.3.2
+ */
 public interface MessageParserFactory {
 
+    /**
+     * 根据连接信息创建消息解析器
+     *
+     * @param connection 连接信息
+     * @return 消息解析器
+     * @see MessageParser
+     * @see MessageParser#builder()
+     */
     Mono<MessageParser> create(ClientConnection connection);
 }
