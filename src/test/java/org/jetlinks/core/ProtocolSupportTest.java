ea21142a629d2304515f03f8f2f8682bd0341590
liusq <441032531@qq.com>
2026-07-17 00:13:58 +0800

    test(protocol): 删除固件元数据低价值测试

--- src/test/java/org/jetlinks/core/ProtocolSupportTest.java
+++ src/test/java/org/jetlinks/core/ProtocolSupportTest.java
@@ -1,16 +1,11 @@
 package org.jetlinks.core;
 
 import org.jetlinks.core.device.TestProtocolSupport;
-import org.jetlinks.core.metadata.FirmwareMetadata;
 import org.jetlinks.core.metadata.FirmwareMetadataContext;
 import org.junit.Test;
 import reactor.test.StepVerifier;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Optional;
-
-import static org.junit.Assert.assertEquals;
 
 public class ProtocolSupportTest {
 
@@ -35,17 +30,5 @@ public class ProtocolSupportTest {
         StepVerifier
             .create(new TestProtocolSupport().parseFirmwareMetadata(context))
             .verifyComplete();
-    }
-
-    @Test
-    public void testFirmwareMetadata() {
-        Map<String, Object> metadata = new LinkedHashMap<>();
-        metadata.put("application", Map.of("releaseVersion", "2.12.0-SNAPSHOT"));
-        FirmwareMetadata firmwareMetadata = new FirmwareMetadata();
-        firmwareMetadata.setVersion("2.12.0-SNAPSHOT");
-        firmwareMetadata.setMetadata(metadata);
-
-        assertEquals("2.12.0-SNAPSHOT", firmwareMetadata.getVersion());
-        assertEquals(metadata, firmwareMetadata.getMetadata());
     }
 }
